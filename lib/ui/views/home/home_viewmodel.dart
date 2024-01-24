f714fcc325418ad13da9fbc601327bcb4c5ef551
Michael Kalango <michaelkalango@gmail.com>
2024-01-24 07:19:26 +0100

    feat: top section

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,25 +1,7 @@
-import 'package:shopping/app/app.dialogs.dart';
-import 'package:shopping/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
+  void actionActivateCredit() {}
 
-  String get counterLabel => 'Counter is: $_counter';
-
-  int _counter = 0;
-
-  void incrementCounter() {
-    _counter++;
-    rebuildUi();
-  }
-
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Stacked Rocks!',
-      description: 'Give stacked $_counter stars on Github',
-    );
-  }
+  void actionSearch() {}
 }
